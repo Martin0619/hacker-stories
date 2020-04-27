04867d940d04dc71423ada9ab8d78c1be33ae94a
Martin0619 <tin.jr0619@gmail.com>
2020-04-27 19:11:01 -0400

    handler functions in jsx

--- src/App.js
+++ src/App.js
@@ -29,30 +29,40 @@ const list = [
 ];
 
 function App() {
+    const handleChange = event => {
+        console.log(event.target.value);
+    };
+
     return (
         <div className="App">
             <h1>{welcome.greeting} {getTittle(welcome.title)}</h1>
             <label htmlFor="search">Search</label>
-            <input id="search" type="text"/>
+            <input
+                id="search"
+                type="text"
+                onChange={handleChange}
+            />
 
             <hr />
 
             {/* render the list here */}
-            {
-                list.map(item => {
-                    return (
-                        <div key={item.objectID}>
-                            <span>
-                                <a href={item.url}>{item.title}</a>
-                            </span>
-                            <span>{item.author}</span>
-                            <span>{item.num_comments}</span>
-                            <span>{item.points}</span>
-                        </div>
-                    );
-                })
-            }
+            <List />
         </div>
+    );
+}
+
+function List() {
+    return list.map(item =>
+        (
+            <div key={item.objectID}>
+                <span>
+                    <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+            </div>
+        )
     );
 }
 
